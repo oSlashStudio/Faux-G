98d3055eeaaee6e486a9de5c24689fbf3676a1f8
Alvian Prasetya <alvian.better@gmail.com>
2016-05-12 23:05:13 +0800

    Implemented basic multiplayer functionalities, removed old assets and objects
    - Implemented basic multiplayer functionalities: server-client HUD,
    movement is now synchronized (mouse movements are not synchronized yet).
    - Removed old assets that were used in previous iterations.

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,39 +1,48 @@
 using UnityEngine;
+using UnityEngine.Networking;
 using System.Collections;
 
-public class PlayerController : MonoBehaviour {
+public class PlayerController : NetworkBehaviour {
 
 	public Attractor attractor;
 	public float moveSpeed = 15.0f;
 	public float jumpForce = 250.0f;
 
+    [SyncVar]
 	private Vector3 movementDirection;
 	private Rigidbody2D rigidBody;
 	private bool canMove = false; // Initially, player is spawned airborne, unable to move
 	private bool canFlip = false; // Initially, player is spawned airborne, unable to flip
 	private bool canJump = false; // Initially, player is spawned airborne, unable to jump
 
+    public override void OnStartLocalPlayer () {
+        Camera.main.GetComponent<CameraController> ().playerObject = gameObject;
+        GetComponent<MeshRenderer> ().material.color = Color.red;
+    }
+
 	// Use this for initialization
 	void Start () {
 		rigidBody = GetComponent<Rigidbody2D> ();
-	}
+    }
 	
 	// Update is called once per frame
 	void Update () {
 		if (canMove) {
-			// Update movement direction based on currently pressed directional button
-			movementDirection = new Vector3 (Input.GetAxisRaw ("Horizontal"), 0.0f, 0.0f).normalized;
-
-			InputFlipGravity ();
-
-			InputJump ();
+            if (!isLocalPlayer) {
+                return;
+            }
+            InputMove ();
+            InputFlipGravity ();
+            InputJump ();
 		}
 	}
 
 	void FixedUpdate () {
 		if (canMove) {
-            Vector3 movementVector = transform.TransformDirection (movementDirection) * moveSpeed * Time.deltaTime;
-            rigidBody.velocity += new Vector2 (movementVector.x, movementVector.y);
+            if (!isLocalPlayer) {
+                return;
+            }
+            Move ();
 		}
 	}
 
@@ -42,6 +51,16 @@ public class PlayerController : MonoBehaviour {
 		canFlip = true;
 		canJump = true;
 	}
+
+    void InputMove () {
+        // Update movement direction based on currently pressed directional button
+        movementDirection = new Vector3 (Input.GetAxisRaw ("Horizontal"), 0.0f, 0.0f).normalized;
+    }
+
+    void Move () {
+        Vector3 movementVector = transform.TransformDirection (movementDirection) * moveSpeed * Time.deltaTime;
+        rigidBody.velocity += new Vector2 (movementVector.x, movementVector.y);
+    }
 
 	void InputFlipGravity () {
 		if (Input.GetKeyDown (KeyCode.Space)) { // Flip gravity
