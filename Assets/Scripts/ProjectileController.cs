12ddf1b4ac65cb26a0284497aad148c23689d2c7
Alvian Prasetya <alvian.better@gmail.com>
2016-05-15 01:13:01 +0800

    Implemented projectile lifetime on all projectiles
    - Implemented projectile lifetime, projectiles will be destroyed upon
    exhausting lifetime (this causes a side effect of projectile range which
    is equivalent to speed * lifetime).

--- Assets/Scripts/ProjectileController.cs
+++ Assets/Scripts/ProjectileController.cs
@@ -5,8 +5,9 @@ using System.Collections;
 public class ProjectileController : NetworkBehaviour {
 
 	public GameObject explosionPrefab;
-	public float bulletSpeed = 5.0f;
-    public float bulletDamage = 10.0f;
+	public float projectileSpeed = 5.0f;
+    public float projectileDamage = 10.0f;
+    public float projectileLifetime = 2.0f;
 	public float effectRange = 20.0f;
 	public float effectIntensity = 0.2f;
 
@@ -14,7 +15,7 @@ public class ProjectileController : NetworkBehaviour {
 
     public override void OnStartServer () {
         rigidBody = GetComponent<Rigidbody2D> ();
-        rigidBody.velocity = new Vector2 (transform.forward.x, transform.forward.y) * bulletSpeed;
+        rigidBody.velocity = new Vector2 (transform.forward.x, transform.forward.y) * projectileSpeed;
     }
 
 	// Use this for initialization
@@ -23,7 +24,19 @@ public class ProjectileController : NetworkBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-
+        projectileLifetime -= Time.deltaTime;
+        if (projectileLifetime <= 0.0f) {
+            if (isClient) {
+                ShakeCamerasInRange ();
+            }
+            if (isServer) {
+                // Instantiate explosion prefab
+                GameObject explosion = (GameObject) Instantiate (explosionPrefab, transform.position, transform.rotation);
+                NetworkServer.Spawn (explosion);
+                // Finally, destroy this game object
+                Destroy (gameObject);
+            }
+        }
 	}
 
 	void OnCollisionEnter2D (Collision2D collision) {
@@ -31,22 +44,13 @@ public class ProjectileController : NetworkBehaviour {
             Physics2D.IgnoreCollision (GetComponent<Collider2D> (), collision.collider);
         } else {
             if (isClient) { // Only simulate camera shake on client due to camera synchronization using Network Transform
-                // Loop through all cameras on scene
-                foreach (Camera currentCamera in Camera.allCameras) {
-                    if (currentCamera.tag.Equals ("MainCamera")) {
-                        Vector3 currentCameraPosition = currentCamera.transform.position;
-                        // Check if distance to currentCamera is within effect distance
-                        if (IsInEffectRange (new Vector2 (currentCameraPosition.x, currentCameraPosition.y))) {
-                            currentCamera.GetComponent<CameraController> ().toggleShaking (effectIntensity);
-                        }
-                    }
-                }
+                ShakeCamerasInRange ();
             }
             if (isServer) { // Only simulate collision event on server due to synchronization using SyncVar and server-side instantiation
                 if (collision.collider.tag.Equals ("Player")) {
                     // Handle damage to player
                     HealthController playerHealthController = collision.gameObject.GetComponent<HealthController> ();
-                    playerHealthController.ReduceHealth (bulletDamage);
+                    playerHealthController.ReduceHealth (projectileDamage);
                 }
                 // Instantiate explosion prefab
                 GameObject explosion = (GameObject) Instantiate (explosionPrefab, transform.position, transform.rotation);
@@ -56,6 +60,19 @@ public class ProjectileController : NetworkBehaviour {
             }
         }
 	}
+
+    void ShakeCamerasInRange () {
+        // Loop through all cameras on scene
+        foreach (Camera currentCamera in Camera.allCameras) {
+            if (currentCamera.tag.Equals ("MainCamera")) {
+                Vector3 currentCameraPosition = currentCamera.transform.position;
+                // Check if distance to currentCamera is within effect distance
+                if (IsInEffectRange (new Vector2 (currentCameraPosition.x, currentCameraPosition.y))) {
+                    currentCamera.GetComponent<CameraController> ().toggleShaking (effectIntensity);
+                }
+            }
+        }
+    }
 
 	/*
 	 * This method checks if a target position is within the specified effect range 
