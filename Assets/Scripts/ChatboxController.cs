1f21d370d6d401f21f6476899019efd1b7b07aa4
Alvian Prasetya <alvian.better@gmail.com>
2016-05-17 00:42:52 +0800

    Fixed bug with client not able to send chat messages
    - Client is now able to send chat messages.
    - Attached chat field controller script to player instead of scene
    object (this fixes the authority bug).

--- Assets/Scripts/ChatboxController.cs
+++ Assets/Scripts/ChatboxController.cs
@@ -8,16 +8,15 @@ public class ChatboxController : NetworkBehaviour {
     public float chatBoxHeight = 160.0f;
     public float chatBoxHorizonalOffset = 20.0f;
     public float chatBoxVerticalOffset = -40.0f;
-    public float chatFieldWidth = 480.0f;
-    public float chatFieldHeight = 20.0f;
-    public float chatFieldHorizontalOffset = 20.0f;
-    public float chatFieldVerticalOffset = -20.0f;
 
     [SyncVar]
-    private string chatBoxString = "";
+    public string chatBoxString = "";
 
-    private bool isChatFieldActive = false;
-    private string chatFieldString = "";
+    public static ChatboxController Instance { get; private set; }
+
+    public override void OnStartServer () {
+        Instance = this;
+    }
 
     // Use this for initialization
     void Start () {
@@ -36,28 +35,6 @@ public class ChatboxController : NetworkBehaviour {
         GUI.SetNextControlName ("ChatBox");
         GUI.Label (new Rect (chatBoxHorizonalOffset, Screen.height - chatBoxHeight + chatBoxVerticalOffset,
             chatBoxWidth, chatBoxHeight), chatBoxString, chatBoxStyle);
-
-        if (Event.current.type == EventType.KeyDown && Event.current.keyCode == KeyCode.Return) {
-            if (!isChatFieldActive) {
-                isChatFieldActive = true;
-            } else {
-                CmdSendChatMessage (chatFieldString);
-                chatFieldString = "";
-                isChatFieldActive = false;
-            }
-        }
-
-        if (isChatFieldActive) {
-            GUI.SetNextControlName ("ChatField");
-            chatFieldString = GUI.TextField (new Rect (chatFieldHorizontalOffset, Screen.height - chatFieldHeight + chatFieldVerticalOffset,
-                chatFieldWidth, chatFieldHeight), chatFieldString, 32);
-            GUI.FocusControl ("ChatField");
-        }
-    }
-
-    [Command]
-    void CmdSendChatMessage (string chatMessage) {
-        chatBoxString = chatMessage + "\n" + chatBoxString;
     }
 
 }
---
+++ Assets/Scripts/PlayerChatFieldController.cs
@@ -0,0 +1,53 @@
+using UnityEngine;
+using UnityEngine.Networking;
+using System.Collections;
+
+public class PlayerChatFieldController : NetworkBehaviour {
+
+    public float chatFieldWidth = 480.0f;
+    public float chatFieldHeight = 20.0f;
+    public float chatFieldHorizontalOffset = 20.0f;
+    public float chatFieldVerticalOffset = -20.0f;
+
+    private bool isChatFieldActive = false;
+    private string chatFieldString = "";
+
+    // Use this for initialization
+    void Start () {
+
+    }
+
+    // Update is called once per frame
+    void Update () {
+
+    }
+
+    void OnGUI () {
+        if (!isLocalPlayer) {
+            return;
+        }
+
+        if (Event.current.type == EventType.KeyDown && Event.current.keyCode == KeyCode.Return) {
+            if (!isChatFieldActive) {
+                isChatFieldActive = true;
+            } else {
+                CmdSendChatMessage (chatFieldString);
+                chatFieldString = "";
+                isChatFieldActive = false;
+            }
+        }
+
+        if (isChatFieldActive) {
+            GUI.SetNextControlName ("ChatField");
+            chatFieldString = GUI.TextField (new Rect (chatFieldHorizontalOffset, Screen.height - chatFieldHeight + chatFieldVerticalOffset,
+                chatFieldWidth, chatFieldHeight), chatFieldString, 32);
+            GUI.FocusControl ("ChatField");
+        }
+    }
+
+    [Command]
+    void CmdSendChatMessage (string chatMessage) {
+        ChatboxController.Instance.chatBoxString = chatMessage + "\n" + ChatboxController.Instance.chatBoxString;
+    }
+
+}
