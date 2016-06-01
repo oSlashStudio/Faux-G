ba30bc2dd82c5f4cb0c1982cd93616c2a700a435
Alvian Prasetya <alvian.better@gmail.com>
2016-06-01 14:07:27 +0800

    Implemented rotating laser as a hazard on Boss Raid Small
    - Rotating laser damages units on its path each frame.

---
+++ Assets/Resources/Scripts/RotatingLaserController.cs
@@ -0,0 +1,45 @@
+using UnityEngine;
+using System.Collections;
+
+public class RotatingLaserController : Photon.MonoBehaviour {
+
+    public GameObject hitEffect;
+
+    public float angularVelocity;
+    public float laserDamage;
+
+    // Cached components
+    private LineRenderer lineRenderer;
+
+	// Use this for initialization
+	void Start () {
+        lineRenderer = GetComponent<LineRenderer> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        
+    }
+
+    void FixedUpdate () {
+        if (photonView.isMine) { // Only master client modifies rotation
+            transform.rotation = Quaternion.Euler (
+                transform.rotation.eulerAngles + new Vector3 (0.0f, 0.0f, angularVelocity * Time.deltaTime)
+                );
+        }
+        RaycastHit2D raycastHit = Physics2D.Raycast (transform.position, transform.rotation * Vector3.up);
+        lineRenderer.SetPosition (1, new Vector3 (raycastHit.point.x, raycastHit.point.y, 0.0f));
+        Instantiate (hitEffect, raycastHit.point, transform.rotation);
+
+        // Handle damage
+        if (raycastHit.rigidbody == null) {
+            return;
+        }
+        HealthController targetHealthController = raycastHit.rigidbody.gameObject.GetComponent<HealthController> ();
+        if (targetHealthController == null) {
+            return;
+        }
+        targetHealthController.Damage (laserDamage, raycastHit.point);
+    }
+
+}
