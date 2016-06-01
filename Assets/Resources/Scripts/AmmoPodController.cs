88cae035cc2614c5ea6de7d6577276e0a22029aa
Alvian Prasetya <alvian.better@gmail.com>
2016-06-01 20:47:11 +0800

    Implemented halo as a visual indicator when a weapon pod is active

--- Assets/Resources/Scripts/AmmoPodController.cs
+++ Assets/Resources/Scripts/AmmoPodController.cs
@@ -7,10 +7,15 @@ public class AmmoPodController : Photon.MonoBehaviour {
     public float defaultSpawnDelay;
 
     private float spawnDelay;
+    private bool hasAmmoPack;
+
+    // Cached components
+    private Component halo;
     private GameObject ammoPack;
 
-	// Use this for initialization
-	void Start () {
+    // Use this for initialization
+    void Start () {
+        halo = GetComponent ("Halo");
         spawnDelay = defaultSpawnDelay;
 	}
 	
@@ -27,12 +32,25 @@ public class AmmoPodController : Photon.MonoBehaviour {
                 spawnDelay = defaultSpawnDelay;
             }
         }
+
+        if (!hasAmmoPack && ammoPack != null) {
+            hasAmmoPack = true;
+            photonView.RPC ("RpcSetHalo", PhotonTargets.All, true);
+        } else if (hasAmmoPack && ammoPack == null) {
+            hasAmmoPack = false;
+            photonView.RPC ("RpcSetHalo", PhotonTargets.All, false);
+        }
 	}
 
     void SpawnAmmo () {
         int spawnedAmmoId = Random.Range (0, ammoPrefabs.Length);
         ammoPack = PhotonNetwork.InstantiateSceneObject (ammoPrefabs[spawnedAmmoId].name, 
             transform.position + transform.up, transform.rotation, 0, null);
+    }
+
+    [PunRPC]
+    void RpcSetHalo (bool flag) {
+        halo.GetType ().GetProperty ("enabled").SetValue (halo, flag, null);
     }
 
 }
