dabe3962d8bc8efeaf589adbf7d32f1c8e927bfe
Alvian Prasetya <alvian.better@gmail.com>
2016-06-04 17:49:36 +0800

    Fixed shotgun fire sound effect being played multiple times

--- Assets/Resources/Scripts/Shotgun.cs
+++ Assets/Resources/Scripts/Shotgun.cs
@@ -11,15 +11,34 @@ public class Shotgun : Weapon {
             for (int i = -numShrapnels / 2; i <= numShrapnels / 2; i++) {
                 Quaternion instantiateRotation = Quaternion.Euler (projectileRotation.eulerAngles + new Vector3 (separationAngle * i, 0.0f, 0.0f));
                 Vector3 instantiatePosition = projectilePosition + instantiateRotation * Vector3.forward * 1.0f;
-                base.Fire (instantiatePosition, instantiateRotation, player, instantiatorId);
+
+                InstantiateShrapnel (instantiatePosition, instantiateRotation, player, instantiatorId);
             }
         } else { // Even number of shrapnels
             for (float i = -numShrapnels / 2 + 0.5f; i <= numShrapnels / 2 - 0.5f; i += 1.0f) {
                 Quaternion instantiateRotation = Quaternion.Euler (projectileRotation.eulerAngles + new Vector3 (separationAngle * i, 0.0f, 0.0f));
                 Vector3 instantiatePosition = projectilePosition + instantiateRotation * Vector3.forward * 1.0f;
-                base.Fire (instantiatePosition, instantiateRotation, player, instantiatorId);
+
+                InstantiateShrapnel (instantiatePosition, instantiateRotation, player, instantiatorId);
             }
         }
+
+        fireDelay = defaultFireDelay;
+        ammo -= 1;
+
+        PlayFireSoundClip ();
+    }
+
+    void InstantiateShrapnel (Vector3 instantiatePosition, Quaternion instantiateRotation, GameObject player, int instantiatorId) {
+        GameObject shrapnel = (GameObject) Instantiate (projectilePrefab, instantiatePosition, instantiateRotation);
+
+        Physics2D.IgnoreCollision (shrapnel.GetComponent<Collider2D> (), player.GetComponent<Collider2D> ());
+
+        // Set projectile owner
+        shrapnel.GetComponent<ProjectileController> ().InstantiatorId = instantiatorId;
+
+        // Set projectile color
+        shrapnel.GetComponent<TrailRenderer> ().material.SetColor ("_TintColor", player.GetComponent<MeshRenderer> ().material.color);
     }
 
 }
