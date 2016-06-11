7f49613a36d7578c410efdcb3036ebcb16f64cbf
Alvian Prasetya <alvian.better@gmail.com>
2016-06-11 14:18:32 +0800

    Implemented global heal ability for Crazy Shaman
    - This ability heals all players on the same team for a certain amount
    of health.

---
+++ Assets/Resources/Scripts/GlobalHeal.cs
@@ -0,0 +1,25 @@
+using UnityEngine;
+using System.Collections;
+using PhotonPlayerExtension;
+
+public class GlobalHeal : Ability {
+
+    public float healAmount;
+
+    protected override void ActivateAbility () {
+        foreach (GameObject player in GameObject.FindGameObjectsWithTag ("Player")) {
+            PhotonView playerPhotonView = player.GetComponent<PhotonView> ();
+
+            if (playerPhotonView.owner.CurrentTeamId () == PhotonNetwork.player.CurrentTeamId ()) {
+                photonView.RPC ("RpcHealTarget", playerPhotonView.owner, playerPhotonView.viewID, PhotonNetwork.player.ID);
+            }
+        }
+    }
+
+    [PunRPC]
+    void RpcHealTarget (int targetViewId, int healingPlayerId) {
+        PhotonView targetPhotonView = PhotonView.Find (targetViewId);
+        targetPhotonView.GetComponent<HealthController> ().Heal (healAmount, healingPlayerId, targetPhotonView.transform.position);
+    }
+
+}
