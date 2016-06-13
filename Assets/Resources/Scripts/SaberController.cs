49585f8e5d14357481f86efcafeb68f8b3afd6ef
Alvian Prasetya <alvian.better@gmail.com>
2016-06-13 18:50:00 +0800

    Changed how the saber slashes work, adjusted slash damage
    - Saber now slashes twice instead of once.
    - Adjusted damage per slash to 25 (non-piercing).

--- Assets/Resources/Scripts/SaberController.cs
+++ Assets/Resources/Scripts/SaberController.cs
@@ -8,21 +8,33 @@ public class SaberController : MonoBehaviour {
     [HideInInspector]
     public int instantiatorId;
 
+    private float lifetime;
+
 	// Use this for initialization
 	void Start () {
-        Destroy (gameObject, 0.25f);
+        lifetime = 0.0f;
 	}
 
     void Update () {
-        transform.RotateAround (transform.parent.position, Vector3.forward, 1440.0f * Time.deltaTime);
+        lifetime += Time.deltaTime;
+        
+        if (lifetime <= 0.25f) {
+            float angularSpeed = Mathf.Lerp (0, 2880, lifetime / 0.25f);
+            transform.RotateAround (transform.parent.position, Vector3.forward, angularSpeed * Time.deltaTime);
+        } else if (lifetime <= 0.5f) {
+            float angularSpeed = Mathf.Lerp (0, 2880, (lifetime - 0.25f) / 0.25f);
+            transform.RotateAround (transform.parent.position, Vector3.forward, -angularSpeed * Time.deltaTime);
+        } else {
+            Destroy (gameObject);
+        }
     }
 
-    void OnTriggerEnter2D (Collider2D collider) {
-        GameObject collidingObject = collider.gameObject;
+    void OnCollisionEnter2D (Collision2D collision) {
+        GameObject collidingObject = collision.gameObject;
         HealthController collidingHealthController = collidingObject.GetComponent<HealthController> ();
 
         if (collidingHealthController != null) {
-            collidingHealthController.Damage (slashDamage, collidingObject.transform.position, isArmorPiercing, instantiatorId);
+            collidingHealthController.Damage (slashDamage, collision.contacts[0].point, isArmorPiercing, instantiatorId);
         }
     }
 
